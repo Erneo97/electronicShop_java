e6bd7e7fb904e8b79bcb1e16b164cc0c283f7193
Erneo97 <michalkaniewski1997@gmail.com>
2026-07-04 12:55:39 +0200

    feat: abstract class to  represent product

---
+++ src/main/java/org/example/model/product/Product.java
@@ -0,0 +1,10 @@
+package org.example.model.product;
+
+import lombok.Data;
+
+@Data
+public abstract class Product {
+    private final String id;
+    private String name, description;
+    private TypeProduct type;
+}
