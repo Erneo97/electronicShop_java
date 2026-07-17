f391d4d5bb66e252df80deefc34f1f528051e03e
Erneo97 <michalkaniewski1997@gmail.com>
2026-07-17 22:56:08 +0200

    feat: make order without validation

--- src/main/java/org/example/service/OrderProcesor.java
+++ src/main/java/org/example/service/OrderProcesor.java
@@ -1,11 +1,39 @@
 package org.example.service;
 
+import org.example.model.order.Order;
+import org.example.model.order.OrderItem;
+import org.example.model.product.Product;
 import org.example.service.repository.ProductRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class OrderProcesor {
-    ProductRepository repository;
+    ProductRepository productRepository;
+    private final List<Order> orders = new ArrayList<>();
 
     public OrderProcesor(ProductRepository productRepository) {
-        this.repository = productRepository;
+        this.productRepository = productRepository;
+    }
+
+    private ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    public Future<Boolean> makeOrder(Order order) {
+        return executorService.submit(() -> {
+            boolean validateResult = validateOrder(order);
+            if (validateResult) {
+                orders.add(order);
+            }
+            return validateResult;
+        });
+    }
+
+    private boolean validateOrder(Order order) {
+
+        return false;
     }
 }
