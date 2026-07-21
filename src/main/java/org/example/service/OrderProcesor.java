ecca9c81460129209db146cdef8e3b71e08a7064
Erneo97 <michalkaniewski1997@gmail.com>
2026-07-21 09:42:40 +0200

    feat: interface to invoice

--- src/main/java/org/example/service/OrderProcesor.java
+++ src/main/java/org/example/service/OrderProcesor.java
@@ -2,13 +2,11 @@ package org.example.service;
 
 import org.example.model.order.Order;
 import org.example.model.order.OrderItem;
-import org.example.model.product.ConfigurationParameter;
 import org.example.model.product.Product;
 import org.example.service.repository.ProductRepository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -16,12 +14,12 @@ import java.util.concurrent.Future;
 public class OrderProcesor {
     ProductRepository productRepository;
     private final List<Order> orders = new ArrayList<>();
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     public OrderProcesor(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     public Future<Boolean> makeOrder(Order order) {
         return executorService.submit(() -> {
