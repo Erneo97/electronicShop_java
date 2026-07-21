5bf45e144ba970a9d7bf0e795d04d6378fa54062
Erneo97 <michalkaniewski1997@gmail.com>
2026-07-21 10:16:10 +0200

    feat: interactive user menu list products show cart exit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.customer.TerminalInterface;
+import org.example.customer.TerminalInterfaceEnum;
 import org.example.model.cart.Cart;
 import org.example.model.product.Product;
 import org.example.service.OrderProcesor;
@@ -8,6 +9,7 @@ import org.example.service.manager.ProductManager;
 import org.example.service.repository.ProductRepository;
 
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -20,20 +22,41 @@ public class Main {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
         TerminalInterface terminal = new TerminalInterface(productManager, orderProcessor);
-        List<Product> products = terminal.getProducts();
-
-        products.forEach(System.out::println);
-
+        Scanner scanner = new Scanner(System.in);
         Cart cart = new Cart();
-        Product testProduct = products.getFirst();
-        cart.addToCart(testProduct, testProduct.getConfiguration());
-        Product testProduc2 = products.get(1);
-        cart.addToCart(testProduc2, testProduc2.getConfiguration());
 
-        System.out.println(cart);
-        Future<Boolean> orderResponse = terminal.makeOrder(cart);
-        System.out.println("Złożenie zamówienia: " + (orderResponse.get(500, TimeUnit.MICROSECONDS) ? "udana" : "Nie udana"));
+        System.out.println("""
+                Sklep internetowy z elektrtoniką
+                \tW czym możemy pomóc?""");
 
-        System.out.println(cart);
+        TerminalInterfaceEnum operation = TerminalInterfaceEnum.LIST_PRODUCT;
+
+        while (operation != TerminalInterfaceEnum.EXIT) {
+            System.out.print("""
+                    Dozwolone komendy w sklepie:
+                    1 - Wyświetl listę produktów
+                    2 - Wyświetl koszyk
+                    15 - Zamknij sklep
+                    Twój wybór:""");
+
+            operation = TerminalInterfaceEnum.from(scanner.nextInt());
+
+            switch (operation) {
+                case LIST_PRODUCT -> terminal.getProducts().forEach(System.out::println);
+                case DISPLAY_CART -> System.out.println(cart);
+                case EXIT -> System.out.println("Dziękujemy za zakupy zapraszamy ponownnie");
+            }
+        }
+
+//        Product testProduct = products.getFirst();
+//        cart.addToCart(testProduct, testProduct.getConfiguration());
+//        Product testProduc2 = products.get(1);
+//        cart.addToCart(testProduc2, testProduc2.getConfiguration());
+//
+//        System.out.println(cart);
+//        Future<Boolean> orderResponse = terminal.makeOrder(cart);
+//        System.out.println("Złożenie zamówienia: " + (orderResponse.get(500, TimeUnit.MICROSECONDS) ? "udana" : "Nie udana"));
+//
+//        System.out.println(cart);
     }
 }
---
+++ src/main/java/org/example/customer/TerminalInterfaceEnum.java
@@ -0,0 +1,24 @@
+package org.example.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+
+import java.util.Arrays;
+
+@AllArgsConstructor
+public enum TerminalInterfaceEnum {
+    LIST_PRODUCT(1),
+    DISPLAY_CART(2),
+    EXIT(15),
+    NOT_RECOGNIZE_OPERATION(-1);
+
+    @NonNull
+    private final int operation;
+
+    public static TerminalInterfaceEnum from(int operation) {
+        return Arrays.stream(TerminalInterfaceEnum.values())
+                .filter(terminalInterfaceEnum ->  terminalInterfaceEnum.operation == operation)
+                .findFirst()
+                .orElse(NOT_RECOGNIZE_OPERATION);
+    }
+}
