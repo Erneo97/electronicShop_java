ee103f84aab694ddcd244cd7d9adeeb9ae61d5b3
Erneo97 <michalkaniewski1997@gmail.com>
2026-07-26 22:23:36 +0200

    test: work of discount

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import org.example.customer.UserCommandLineInterface;
+import org.example.model.cart.Cart;
+import org.example.model.discount.Discount;
+import org.example.model.invoice.Invoice;
+import org.example.model.invoice.PolishInvoice;
+import org.example.model.product.Product;
+import org.example.model.product.TypeProduct;
+import org.example.service.OrderProcesor;
+import org.example.service.manager.ProductManager;
+import org.example.service.repository.ProductRepository;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+public class Test {
+    private static final ProductRepository productRepository = new ProductRepository();
+    private static final ProductManager productManager = new ProductManager(productRepository);
+    private static final OrderProcesor orderProcessor = new OrderProcesor(productRepository);
+    private static final Invoice polishInvoice = new PolishInvoice();
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        UserCommandLineInterface terminal = new UserCommandLineInterface(productManager, orderProcessor, polishInvoice);
+        Cart cart = new Cart();
+
+        Discount minus20PercentForAllSmartphones = new Discount(p -> p.getType() == TypeProduct.SMARTPHONE,
+                p -> p.getTotalPrice()
+                        .subtract(p.getTotalPrice().multiply(BigDecimal.valueOf(0.2))
+                        ));
+
+        Product first = terminal.getProducts().get(0);
+
+        System.out.println("Cena przed obniżką: " + first.getTotalPrice());
+        System.out.println("Cena po obniżce : " + minus20PercentForAllSmartphones.applyDiscount(first));
+    }
+}
