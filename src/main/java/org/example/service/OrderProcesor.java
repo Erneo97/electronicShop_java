25f8849d397e6d4869a2255a50e12b214e2412a9
Erneo97 <michalkaniewski1997@gmail.com>
2026-07-26 23:40:32 +0200

    refactor: validate quantity of parameters in product

--- src/main/java/org/example/service/OrderProcesor.java
+++ src/main/java/org/example/service/OrderProcesor.java
@@ -1,10 +1,10 @@
 package org.example.service;
 
 import org.example.model.order.Order;
-import org.example.model.order.exeptions.OrderCannotFulfiledExeption;
 import org.example.model.order.OrderItem;
 import org.example.model.order.ParameterOfOrder;
 import org.example.model.order.exeptions.ProductNotExists;
+import org.example.model.order.exeptions.SelectedParametersNotAvaliableExeption;
 import org.example.model.product.Product;
 import org.example.service.repository.ProductRepository;
 
@@ -14,7 +14,6 @@ import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.stream.Stream;
 
 public class OrderProcesor {
     private final ProductRepository productRepository;
@@ -34,39 +33,36 @@ public class OrderProcesor {
         });
     }
 
-    private void validateOrder(Order order) throws ProductNotExists {
-        validateProductsInOrdeExistsr(order);
+    private void validateOrder(Order order) throws ProductNotExists, SelectedParametersNotAvaliableExeption {
+        for (OrderItem orderItem : order.products()) {
+            Product existedProduct = validateProductsInOrderAndGet(orderItem);
+            validateAvailableConfigurationProduct(orderItem, existedProduct);
+        }
 
     }
 
-    private void validateProductsInOrdeExistsr(Order order) throws ProductNotExists {
-        for( OrderItem orderItem : order.products()) {
-            Optional<Product> optProduct = productRepository.getProductById(orderItem.idProduct());
-            if(optProduct.isEmpty()) {
-                throw new ProductNotExists(String.format("Produkt z id %s nie znaleziony", orderItem.idProduct()));
+    private Product validateProductsInOrderAndGet(OrderItem orderItem) throws ProductNotExists {
+        Optional<Product> optProduct = productRepository.getProductById(orderItem.idProduct());
+        if (optProduct.isEmpty()) {
+            throw new ProductNotExists(String.format("Produkt z id %s nie znaleziony", orderItem.idProduct()));
+        }
+        return optProduct.get();
+    }
+
+
+    private void validateAvailableConfigurationProduct(OrderItem orderedProduct, Product product) throws SelectedParametersNotAvaliableExeption {
+        for (ParameterOfOrder parameterOfOrder : orderedProduct.parameters()) {
+            if (!validateQuantityParametersProduct(product, parameterOfOrder)) {
+                throw new SelectedParametersNotAvaliableExeption(
+                        String.format("Product id: %s nie posiada wystarczającej ilości konfiguracji o id %s", product.getId(), parameterOfOrder.idParameter())
+                );
             }
         }
     }
 
-    private boolean validateOrderItemCorrectness(OrderItem orderProduct) {
-        return productRepository.getProductById(orderProduct.idProduct())
-//               TODO: sprawdzenie czy
-                .map(product -> validateAvailableConfigurationProduct(orderProduct, product))
-                .orElse(false);
-    }
-
-    private boolean validateAvailableConfigurationProduct(OrderItem orderedProduct, Product product) {
-        return getParameterForOrderItems(orderedProduct)
-                .allMatch(parameter -> validateQuantityParametersProduct(product, parameter));
-    }
-
     private boolean validateQuantityParametersProduct(Product product, ParameterOfOrder parameter) {
-        return  product.getConfigurationById(parameter.idParameter())
+        return product.getConfigurationById(parameter.idParameter())
                 .map(configuration -> configuration.getQuantity() >= parameter.quantity())
                 .orElse(false);
-    }
-
-    private Stream<ParameterOfOrder> getParameterForOrderItems(OrderItem orderedProduct) {
-        return orderedProduct.parameters().stream();
     }
 }
